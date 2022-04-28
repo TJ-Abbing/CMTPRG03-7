92327182cd0a357be2033b0eb7a30ff50ac80c77
TJ-Abbing <tjabbing@gmail.com>
2022-04-28 14:16:53 +0200

    map

--- Case 1/script.js
+++ Case 1/script.js
@@ -1,22 +1,15 @@
 const { fetch } = require('native-fetch')
 
-// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-//     .then(res => res.text())
-//     .then(body => console.log(body))
-
 async function loadPokemonData(){
     try {
         const result = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=3')
         const data = await result.json();
-        console.log(data);
+        data.results.map((pokemon) => {
+            console.log(pokemon.name)
+        })
     } catch (error) {
         console.log('error', error);
     }
 }
 loadPokemonData();
-
-// const loadPokemonData = loadPokemonData.map(pokemon => {
-//     return pokemon 
-// })
-// console.log(loadPokemonData)
 
